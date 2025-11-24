8a415a85f5281ba4efb541af91141812c2a85c23
rasel739 <raselhossaintro@gmail.com>
2025-11-24 12:33:08 +0600

    auth: authentication is completed

--- src/app/modules/auth/auth.interface.ts
+++ src/app/modules/auth/auth.interface.ts
@@ -1,5 +1,5 @@
 export interface IUser {
-  name: string;
+  fullName: string;
   email: string;
   password: string;
 }
--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -1,10 +1,10 @@
 import { z } from 'zod';
 
-const userZodSchema = z.object({
+export const userZodSchema = z.object({
   body: z.object({
-    name: z.string().min(2, 'Name must be at least 2 characters'),
-    email: z.string({ error: 'Email is required' }),
-    password: z.string().min(6, 'Password must be at least 6 characters'),
+    fullName: z.string().min(1, 'First name is required').max(100),
+    email: z.string('Email address is required'),
+    password: z.string().min(6, 'Password must be at least 8 characters'),
   }),
 });
 
